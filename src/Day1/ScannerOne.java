7c244061e238309d33bfe4544275c5e816560bfb
dejahmartin <dejah1520@gmail.com>
2021-10-28 11:55:16 -0500

    DayOne Scanner

---
+++ src/Day1/ScannerOne.java
@@ -0,0 +1,9 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class ScannerOne {
+    public static void main(String[] args) {
+        java.util.Scanner scan = new java.util.Scanner(System.in);
+    }
+}
